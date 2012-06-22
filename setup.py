44a21faca6e09aa270954c872cf5395cacba76ed
sammyrulez <sammyrulez@gmail.com>
2012-06-22 10:37:05 +0200

    changed version and licence

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='diablo_api_py',
-    version='0.0.1',
+    version='0.0.2',
     description='Python client for Diablo 3 web api',
     author='sammyrulez',
     url='https://github.com/sammyrulez/diablo-api-py',
-    license='BSD',
+    license='MIT',
     packages=find_packages(exclude=('tests')),
     py_modules=['diablo'],
     install_requires=['requests']
